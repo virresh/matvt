c02f501f2b75c3161db8d4cba4a0df53149f4262
Viresh Gupta <virresh@users.noreply.github.com>
2021-01-29 19:35:43 +0530

    Basic Interface
    Need to wire with proper key event dispersion mechanism

--- app/src/main/java/io/github/virresh/matvt/services/MouseEventService.java
+++ app/src/main/java/io/github/virresh/matvt/services/MouseEventService.java
@@ -6,9 +6,17 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
 
+import io.github.virresh.matvt.engine.impl.MouseEmulationEngine;
+import io.github.virresh.matvt.view.OverlayView;
+
 public class MouseEventService extends AccessibilityService {
 
     private static String LOG_TAG = "MATVT_SERVICE";
+    private static MouseEventService sMouseEventService;
+    private boolean mStarted = false;
+    private MouseEmulationEngine mEngine;
+    private OverlayView mOverlayView;
+    private boolean mCursorCapture = false;
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
@@ -18,6 +26,9 @@ public class MouseEventService extends AccessibilityService {
     @Override
     protected boolean onKeyEvent(KeyEvent event) {
         Log.i(LOG_TAG, event.toString());
+        if (mCursorCapture) {
+            return mEngine.perform(event);
+        }
         return super.onKeyEvent(event);
     }
 
@@ -26,13 +37,37 @@ public class MouseEventService extends AccessibilityService {
 
     }
 
+    /**
+     * The service is turned on
+     */
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
         Log.i(LOG_TAG, "TV Service Connected!");
-//        AccessibilityServiceInfo eventConnectionInfo = new AccessibilityServiceInfo();
-//        eventConnectionInfo.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
-//        this.setServiceInfo(eventConnectionInfo);
-//        Log.i(LOG_TAG, "Setting event types to " + eventConnectionInfo.eventTypes);
+        init();
+    }
+
+    private void init() {
+        if (mStarted) {
+            Log.i(LOG_TAG, "MouseEventService is already running");
+            return;
+        }
+
+        sMouseEventService = this;
+        mOverlayView = new OverlayView(this);
+        initEngine();
+    }
+
+    private void initEngine() {
+        if (mEngine != null) {
+            Log.i(LOG_TAG, "Mouse Emulation Engine already running");
+        }
+        mEngine = new MouseEmulationEngine(this, mOverlayView);
+        Log.i(LOG_TAG, "Overlay Engine W, H " + mOverlayView.getWidth() + " " + mOverlayView.getHeight());
+        mEngine.init(this);
+    }
+
+    public void setCursorCapture(boolean val) {
+        mCursorCapture = val;
     }
 }
---
+++ app/src/main/java/io/github/virresh/matvt/view/OverlayView.java
@@ -0,0 +1,51 @@
+package io.github.virresh.matvt.view;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+
+public class OverlayView extends RelativeLayout {
+    private static String LOG_TAG = "MATVT Overlay";
+
+    public OverlayView(Context context) {
+        super(context);
+
+        WindowManager.LayoutParams overlayParams = new WindowManager.LayoutParams();
+
+        overlayParams.setTitle("Overlay For MATVT mouse cursor");
+
+        overlayParams.format = PixelFormat.TRANSLUCENT;
+
+        overlayParams.type = WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY |
+                            WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY;
+
+        overlayParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+
+        overlayParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        overlayParams.height = WindowManager.LayoutParams.MATCH_PARENT;
+
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        wm.addView(this, overlayParams);
+    }
+
+    void cleanup() {
+        WindowManager wm = (WindowManager) this.getContext().getSystemService(Context.WINDOW_SERVICE);
+        wm.removeViewImmediate(this);
+
+        Log.i(LOG_TAG, "Overlay view has been removed");
+    }
+
+    public void addFullScreenLayer (View v) {
+        RelativeLayout.LayoutParams lp= new RelativeLayout.LayoutParams(this.getWidth(), this.getHeight());
+        lp.width= RelativeLayout.LayoutParams.MATCH_PARENT;
+        lp.height= RelativeLayout.LayoutParams.MATCH_PARENT;
+
+        v.setLayoutParams(lp);
+        this.addView(v);
+        Log.i("Overlay View", "W - H : " + this.getWidth() + " " + this.getHeight());
+    }
+}
